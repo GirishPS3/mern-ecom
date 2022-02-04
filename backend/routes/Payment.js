bcc007a8ecc7ac07bf7b24b8b1d9285007284cd4
girish <psgirish96@gmail.com>
2022-02-04 09:36:45 +0530

    relative path fix

--- backend/routes/Payment.js
+++ backend/routes/Payment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendStripeApiKey, processPayment } = require('../controllers/payment');
+const { sendStripeApiKey, processPayment } = require('../controllers/Payment');
 
 router.get('/stripeApiKey', sendStripeApiKey);
 router.post('/process', processPayment);
